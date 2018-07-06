1e284fb02e17c118d1ec5c72882e136749e59617
github username <github email address>
2018-07-06 14:36:34 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,5 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food.find do |type|
-  cheese_types.include?(type)
-end
+  food.find {|type| cheese_types.include?(type)}
 end
